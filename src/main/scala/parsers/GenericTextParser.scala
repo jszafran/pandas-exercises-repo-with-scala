a8302a0b2e50a9b22700a763c61d78c6442ea40c
jszafran <jszafran.pv@gmail.com>
2021-11-28 21:20:00 +0100

    Favour map over for expression.

--- src/main/scala/parsers/GenericTextParser.scala
+++ src/main/scala/parsers/GenericTextParser.scala
@@ -9,6 +9,6 @@ object GenericTextParser {
                   ): List[T] = {
     val source = io.Source.fromFile(filePath)
     val lines = if (skipHeader) {source.getLines.drop(1)} else {source.getLines}
-    (for (line <- lines) yield lineParsingFunc(line)).toList
+    lines.map(lineParsingFunc).toList
   }
 }
