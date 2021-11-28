1decc1300337431a0af56cce3d3c1f4564b7a343
jszafran <jszafran.pv@gmail.com>
2021-11-28 10:17:48 +0100

    Adding answers to first 2 questions for Euro.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 I try to solve small data puzzles from this repo ([Pandas exercises](https://github.com/guipsamora/pandas_exercises) - pandas being data manipulation library for Python) using Scala collections.
 
-To make it a better Scala exercise, I'm trying to avoid using vars :-).
+No `var` keyword allowed, only `val` ;).
--- src/main/scala/filteringandsorting/euro12/Euro12.scala
+++ src/main/scala/filteringandsorting/euro12/Euro12.scala
@@ -3,7 +3,22 @@ package filteringandsorting.euro12
 
 import parsers.EuroTeamsDataParser
 
+import dev.jszafran.utils.printBreak
+
 object Euro12 extends App {
   val teams = EuroTeamsDataParser.parseData("./datasets/Euro_2012_stats_TEAM.csv")
-  println(teams.head)
+
+  // Q1
+  println("Q: How many team participated in the Euro2012?")
+  assert(teams.map(_.name).distinct.length == teams.length)
+  println(s"A: ${teams.length} teams participated in Euro 2012.")
+  printBreak()
+
+  // Q2
+  println("Q: Sort the teams by Red Cards, then to Yellow Cards")
+  val teamsSortedByCards = teams
+    .sortBy(t => (t.redCards, t.yellowCards))
+    .map(t => (t.name, t.redCards, t.yellowCards))
+  teamsSortedByCards.foreach(println)
+  printBreak()
 }
