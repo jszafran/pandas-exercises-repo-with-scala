3982f2956e170ab464f7ebd3551569ecc9045f95
jszafran <jszafran.pv@gmail.com>
2021-11-28 10:34:22 +0100

    Finishing Euro 2012 dataset questions.

--- src/main/scala/filteringandsorting/euro12/Euro12.scala
+++ src/main/scala/filteringandsorting/euro12/Euro12.scala
@@ -18,7 +18,39 @@ object Euro12 extends App {
   println("Q: Sort the teams by Red Cards, then to Yellow Cards")
   val teamsSortedByCards = teams
     .sortBy(t => (t.redCards, t.yellowCards))
-    .map(t => (t.name, t.redCards, t.yellowCards))
+    .map(t => (t.name, t.redCards, t.yellowCards)).reverse
   teamsSortedByCards.foreach(println)
   printBreak()
+
+  // Q3
+  println("Q: Calculate the mean Yellow Cards given per Team")
+  val yellowCardsMeanPerTeam = teams.map(_.yellowCards).sum / teams.length
+  println(yellowCardsMeanPerTeam)
+  printBreak()
+
+  // Q4
+  println("Q: Filter teams that scored more than 6 goals")
+  val teamsWithMoreThan6Goals = teams
+    .filter(_.goals > 6)
+    .map(t => (t.name, t.goals))
+
+  teamsWithMoreThan6Goals.foreach(println)
+  printBreak()
+
+  // Q5
+  println("Q: Select the teams that start with G")
+  val teamsStartingWithGLetter = teams
+    .map(_.name)
+    .filter(_.startsWith("G"))
+
+  teamsStartingWithGLetter.foreach(println)
+  printBreak()
+
+  // Q7
+  println("Q: Present only the Shooting Accuracy from England, Italy and Russia")
+  val accuracyForSelectedCountries =  teams
+    .filter(t => Set("England", "Italy", "Russia").contains(t.name))
+    .map(t => (t.name, t.shootingAccuracy))
+
+  accuracyForSelectedCountries.foreach(println)
 }
