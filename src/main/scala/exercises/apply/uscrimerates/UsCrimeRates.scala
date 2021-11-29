3ce57b80696686205dccf748cc1941587bf07958
jszafran <jszafran.pv@gmail.com>
2021-11-29 20:58:23 +0100

    Adding questions that make sense for US Crime rates.

--- src/main/scala/exercises/apply/uscrimerates/UsCrimeRates.scala
+++ src/main/scala/exercises/apply/uscrimerates/UsCrimeRates.scala
@@ -5,5 +5,11 @@ import parsers.USCrimeRatesParser
 
 object UsCrimeRates extends App {
   val crimeRates = USCrimeRatesParser.parseData("./datasets/US_Crime_Rates_1960_2014.csv")
-  println(crimeRates.head)
+
+  // Q1
+  println("Q: Group the year by decades and sum the values")
+
+  // Q2
+ println("Q: What is the most dangerous decade to live in the US?")
+
 }
