b113803ef1334cfe0f5108bd7694e086f6b80e0c
jszafran <jszafran.pv@gmail.com>
2021-11-29 08:04:39 +0100

    Finishing Grouping -> Fictional Regiments data questions.

--- src/main/scala/exercises/grouping/regiment/Regiment.scala
+++ src/main/scala/exercises/grouping/regiment/Regiment.scala
@@ -3,8 +3,43 @@ package exercises.grouping.regiment
 
 import parsers.RegimentTestResultsParser
 
+import utils.printBreak
+
 object Regiment extends App {
   val soldiersResults = RegimentTestResultsParser.parseData("./datasets/fictional_army_test_results.csv")
 
-  println(soldiersResults.head)
+  // Q1
+  println("Q: What is the mean preTestScore from the regiment Nighthawks?")
+  val q1Answer = soldiersResults
+    .filter(_.regiment == "Nighthawks")
+    .map(_.preTestScore).sum / soldiersResults.count(_.regiment == "Nighthawks").toDouble
+  println(q1Answer)
+  printBreak()
+
+  // Q2
+  println("Q: What is the mean of each company's preTestScore?")
+  val q2Answer = soldiersResults
+    .groupBy(_.companyName)
+    .transform((k, v) => v.map(_.preTestScore).sum / v.length.toDouble)
+  q2Answer.foreach(println)
+  printBreak()
+
+  // Q3
+  println("Q: Present the mean preTestScores grouped by regiment and company")
+  val q3Answer = soldiersResults
+    .groupBy(sr => (sr.regiment, sr.companyName))
+    .transform((k, v) => v.map(_.preTestScore).sum / v.length.toDouble)
+    .toSeq
+    .sortBy(_._1)
+  q3Answer.foreach(println)
+  printBreak()
+
+  // Q4
+  println("Q: What is the number of observations in each regiment and company")
+  val q4Answer = soldiersResults
+    .groupBy(sr => (sr.regiment, sr.companyName))
+    .transform((k, v) => v.length)
+    .toSeq
+    .sortBy(_._1)
+  q4Answer.foreach(println)
 }
